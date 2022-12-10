1e7a437a6033788aacf893182d198f012b7552c5
William Rummler <w.a.rummler@gmail.com>
2022-12-09 20:52:55 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+public class ABCDRetrieval
+{
+    private static double[] ABCD() => new double[] { 1, 2, 3, 4 };
+
+    [Benchmark]
+    public double ListPattern()
+    {
+        var abcd = ABCD();
+        if (abcd is [var a, var b, var c, var d])
+        {
+            return a + b + c + d;
+        }
+        else
+        {
+            throw new Exception("Can't happen.");
+        }
+    }
+
+    [Benchmark]
+    public double ArrayAccess()
+    {
+        var abcd = ABCD();
+        var (a, b, c, d) = (abcd[0], abcd[1], abcd[2], abcd[3]);
+        return a + b + c + d;
+    }
+}
